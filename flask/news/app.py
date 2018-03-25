85356470678bd23911800caddadae052775f5b35
monster-shang <jshang@live.cn>
2018-03-26 01:33:48 +0800

    tiaozhan6

--- flask/news/app.py
+++ flask/news/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
-from flask import Flask,render_template
-import os
+from flask import Flask,render_template,abort
+import os,json
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 @app.route('/')
@@ -10,7 +10,14 @@ def index():
     return render_template('index.html',list=list)
 @app.route('/files/<filename>')
 def file(filename):
-    return render_template('file.html',filename=filename)
+    if not os.path.exists('/home/shiyanlou/shiyanlou/flask/files/'+filename+'.json'):
+        abort(404)
+    with open('/home/shiyanlou/shiyanlou/flask/files/'+filename+'.json','r') as file:
+        txt = json.loads(file.read())
+    title = txt['title']
+    content = txt['content']
+    created_time = txt['created_time']
+    return render_template('file.html',filename=filename,title=title,content=content,created_time=created_time)
 @app.errorhandler(404)
 def not_found(error):
-    return render_template('404.html')
+    return render_template('404.html'),404
