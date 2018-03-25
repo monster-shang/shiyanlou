3299f8c861abe732f02a8322c976203319b37a43
monster-shang <jshang@live.cn>
2018-03-25 22:20:14 +0800

    week2

--- flask/news/app.py
+++ flask/news/app.py
@@ -1,9 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
 from flask import Flask,render_template
+import os
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 @app.route('/')
 def index():
-    return render_template('index.html')
+    list = os.listdir('/home/shiyanlou/shiyanlou/flask/files')
+    return render_template('index.html',list=list)
 @app.route('/files/<filename>')
 def file(filename):
     return render_template('file.html',filename=filename)
