f5b24370d979500914f7b256e4b7a53d282ca9df
monster-shang <jshang@live.cn>
2018-04-03 03:59:36 +0800

    tiaozhan10

--- week3/mongodb/getrank.py
+++ week3/mongodb/getrank.py
@@ -3,17 +3,27 @@
 
 import sys
 from pymongo import MongoClient
-
 def get_rank(user_id):
-    client = MongoClient()
+    client = MongoClient('127.0.0.1',27017)
     db = client.shiyanlou
     contests = db.contests
-
-    
+    userdata = list(contests.aggregate([{'$match':{'user_id':user_id}},{'$group' : {'_id' : user_id, 'score' : {'$sum' : "$score"}, 'time' : {'$sum' : "$submit_time"}}}]))
+    if len(userdata) == 0:
+        return 0,0,0
+    data = userdata[0]
+    userdata = list(contests.aggregate([{'$group':{'_id':'$user_id','score':{'$sum':'$score'},'time':{'$sum':'$submit_time'}}},{'$match':{'$or':[{'score':{'$gt':data['score']}},{'time':{'$lt':data['time']},'score':data['score']}]}},{'$group':{'_id':None,'count':{'$sum':1}}}]))
+    if len(userdata) > 0 :
+        rank = userdata[0]['count']+1
+    else:
+        rank = 1
+    score = data['score']
+    submit_time = data['time']
     return rank, score, submit_time
 
 if __name__ == '__main__':
-    
-    user_1 = sys.argv[1:]
-    userdata = get_rank(user_id)
+    if len(sys.argv) != 2:
+        print("Parameter error.")
+        sys.exit()
+    user_id = sys.argv[1]
+    userdata = get_rank(int(user_id))
     print(userdata)
