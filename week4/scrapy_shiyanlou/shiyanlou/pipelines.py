a8f4f3dd5ae3b9f0bc593f5fc91c486f8bd9e40f
monster-shang <jsahng@live.cn>
2018-04-09 11:20:18 +0800

    tiaozhan16

--- week4/scrapy_shiyanlou/shiyanlou/pipelines.py
+++ week4/scrapy_shiyanlou/shiyanlou/pipelines.py
@@ -6,16 +6,25 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 from sqlalchemy.orm import sessionmaker
-from shiyanlou.models import Course,engine
+from shiyanlou.models import Course,engine,User
 from scrapy.exceptions import DropItem
+from shiyanlou.items import CourseItem,UserItem
+from datetime import datetime
 class ShiyanlouPipeline(object):
     def process_item(self, item, spider):
-        item['students'] = int(item['students'])
-        if item['students'] < 1000:
-            raise DropItem('Course students less than 1000.')
+        if isinstance(item,CourseItem):
+            self._process_course_item(item)
         else:
-            self.session.add(Course(**item))
-            return item
+            self._process_user_item(item)
+        return item
+    def _process_course_item(self,item):
+        item['students'] = int(item['students'])
+        self.session.add(Course(**item))
+    def _process_user_item(self,item):
+        item['level'] = int(item['level'][1:])
+        item['join_date'] = datetime.strptime(item['join_date'].split()[0],'%Y-%m-%d').date()
+        item['learn_courses_num'] = int(item['learn_courses_num'])
+        self.session.add(User(**item))
     def open_spider(self,spider):
         Session = sessionmaker(bind=engine)
         self.session = Session()
