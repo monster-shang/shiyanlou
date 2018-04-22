97a19c54dc9f6bc966be517ff0cf3d3b33104a99
monster-shang <jshang@live.cn>
2018-04-22 12:35:20 +0800

    tiaozhan19

---
+++ week5/flask_doc/flask_doc/spiders/flask.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+from flask_doc.items import FlaskDocItem
+
+
+class FlaskSpider(CrawlSpider):
+    name = 'flask'
+    allowed_domains = ['flask.pocoo.org']
+    start_urls = ['http://http://flask.pocoo.org/docs/0.12/']
+
+    rules = (
+        Rule(LinkExtractor(allow=r'http://http://flask.pocoo.org/docs/0.12/.*'), callback='parse_item', follow=True),
+    )
+
+    def parse_item(self, response):
+        item = FlaskDocItem()
+        item['url'] = response.url
+        item['text'] = response.xpath('//text()').extract()
+        yield item
+        
